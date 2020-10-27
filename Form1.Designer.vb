f7d6b4deff903a2d5833e325e97f29faa6a29783
JA <JA@JA>
2020-10-27 15:47:11 +0800

    First Commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,249 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.btn6 = New System.Windows.Forms.Button()
+        Me.btn7 = New System.Windows.Forms.Button()
+        Me.btn8 = New System.Windows.Forms.Button()
+        Me.btn9 = New System.Windows.Forms.Button()
+        Me.btnClick = New System.Windows.Forms.Button()
+        Me.btnMin = New System.Windows.Forms.Button()
+        Me.btnPlus = New System.Windows.Forms.Button()
+        Me.btnMul = New System.Windows.Forms.Button()
+        Me.btnDiv = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btn1
+        '
+        Me.btn1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btn1.Location = New System.Drawing.Point(40, 307)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(56, 56)
+        Me.btn1.TabIndex = 0
+        Me.btn1.Text = "1"
+        Me.btn1.UseVisualStyleBackColor = True
+        '
+        'btn2
+        '
+        Me.btn2.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btn2.Location = New System.Drawing.Point(117, 307)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(56, 56)
+        Me.btn2.TabIndex = 1
+        Me.btn2.Text = "2"
+        Me.btn2.UseVisualStyleBackColor = True
+        '
+        'btn3
+        '
+        Me.btn3.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btn3.Location = New System.Drawing.Point(191, 307)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(56, 56)
+        Me.btn3.TabIndex = 2
+        Me.btn3.Text = "3"
+        Me.btn3.UseVisualStyleBackColor = True
+        '
+        'btn4
+        '
+        Me.btn4.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btn4.Location = New System.Drawing.Point(40, 245)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(56, 56)
+        Me.btn4.TabIndex = 3
+        Me.btn4.Text = "4"
+        Me.btn4.UseVisualStyleBackColor = True
+        '
+        'btn5
+        '
+        Me.btn5.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btn5.Location = New System.Drawing.Point(117, 245)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(56, 56)
+        Me.btn5.TabIndex = 4
+        Me.btn5.Text = "5"
+        Me.btn5.UseVisualStyleBackColor = True
+        '
+        'btn6
+        '
+        Me.btn6.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btn6.Location = New System.Drawing.Point(191, 245)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(56, 56)
+        Me.btn6.TabIndex = 5
+        Me.btn6.Text = "6"
+        Me.btn6.UseVisualStyleBackColor = True
+        '
+        'btn7
+        '
+        Me.btn7.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btn7.Location = New System.Drawing.Point(40, 183)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(56, 56)
+        Me.btn7.TabIndex = 6
+        Me.btn7.Text = "7"
+        Me.btn7.UseVisualStyleBackColor = True
+        '
+        'btn8
+        '
+        Me.btn8.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btn8.Location = New System.Drawing.Point(117, 183)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(56, 56)
+        Me.btn8.TabIndex = 7
+        Me.btn8.Text = "8"
+        Me.btn8.UseVisualStyleBackColor = True
+        '
+        'btn9
+        '
+        Me.btn9.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btn9.Location = New System.Drawing.Point(191, 183)
+        Me.btn9.Name = "btn9"
+        Me.btn9.Size = New System.Drawing.Size(56, 56)
+        Me.btn9.TabIndex = 8
+        Me.btn9.Text = "9"
+        Me.btn9.UseVisualStyleBackColor = True
+        '
+        'btnClick
+        '
+        Me.btnClick.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnClick.Location = New System.Drawing.Point(40, 369)
+        Me.btnClick.Name = "btnClick"
+        Me.btnClick.Size = New System.Drawing.Size(56, 56)
+        Me.btnClick.TabIndex = 9
+        Me.btnClick.Text = "0"
+        Me.btnClick.UseVisualStyleBackColor = True
+        '
+        'btnMin
+        '
+        Me.btnMin.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnMin.Location = New System.Drawing.Point(263, 121)
+        Me.btnMin.Name = "btnMin"
+        Me.btnMin.Size = New System.Drawing.Size(56, 56)
+        Me.btnMin.TabIndex = 11
+        Me.btnMin.Text = "-"
+        Me.btnMin.UseVisualStyleBackColor = True
+        '
+        'btnPlus
+        '
+        Me.btnPlus.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnPlus.Location = New System.Drawing.Point(263, 183)
+        Me.btnPlus.Name = "btnPlus"
+        Me.btnPlus.Size = New System.Drawing.Size(56, 118)
+        Me.btnPlus.TabIndex = 12
+        Me.btnPlus.Text = "+"
+        Me.btnPlus.UseVisualStyleBackColor = True
+        '
+        'btnMul
+        '
+        Me.btnMul.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnMul.Location = New System.Drawing.Point(191, 121)
+        Me.btnMul.Name = "btnMul"
+        Me.btnMul.Size = New System.Drawing.Size(56, 56)
+        Me.btnMul.TabIndex = 14
+        Me.btnMul.Text = "X"
+        Me.btnMul.UseVisualStyleBackColor = True
+        '
+        'btnDiv
+        '
+        Me.btnDiv.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnDiv.Location = New System.Drawing.Point(117, 121)
+        Me.btnDiv.Name = "btnDiv"
+        Me.btnDiv.Size = New System.Drawing.Size(56, 56)
+        Me.btnDiv.TabIndex = 15
+        Me.btnDiv.Text = "/"
+        Me.btnDiv.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnReset.Location = New System.Drawing.Point(117, 369)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(130, 56)
+        Me.btnReset.TabIndex = 18
+        Me.btnReset.Text = "Clear"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Enabled = False
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(40, 71)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(279, 29)
+        Me.TextBox1.TabIndex = 19
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.ClientSize = New System.Drawing.Size(364, 467)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnDiv)
+        Me.Controls.Add(Me.btnMul)
+        Me.Controls.Add(Me.btnPlus)
+        Me.Controls.Add(Me.btnMin)
+        Me.Controls.Add(Me.btnClick)
+        Me.Controls.Add(Me.btn9)
+        Me.Controls.Add(Me.btn8)
+        Me.Controls.Add(Me.btn7)
+        Me.Controls.Add(Me.btn6)
+        Me.Controls.Add(Me.btn5)
+        Me.Controls.Add(Me.btn4)
+        Me.Controls.Add(Me.btn3)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.btn1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btnClick As Button
+    Friend WithEvents btnMin As Button
+    Friend WithEvents btnPlus As Button
+    Friend WithEvents btnMul As Button
+    Friend WithEvents btnDiv As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,123 @@
+﻿Public Class Form1
+
+    Dim n1, n2 As Double
+    Dim oprClickCount As Integer = 0
+    Dim isOPrClick As Boolean = False
+    Dim isEqualClick As Boolean = False
+    Dim opr As String
+
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        For Each c As Control In Controls
+
+            If c.GetType() = GetType(Button) Then
+                If Not c.Text.Equals("reset") Then
+                    AddHandler c.Click, AddressOf Btn_Click
+                End If
+            End If
+        Next
+    End Sub
+
+
+    Private Sub Btn_Click(sender As Object, e As EventArgs)
+        Dim button As Button = sender
+
+
+        If Not IsOperator(button) Then
+            If isOPrClick Then
+                n1 = Double.Parse(TextBox1.Text)
+                TextBox1.Text = ""
+            End If
+            If Not TextBox1.Text.Contains(".") Then
+                If TextBox1.Text.Equals("0") AndAlso Not button.Text.Equals(".") Then
+                    TextBox1.Text = button.Text
+                    isOPrClick = False
+                Else
+                    TextBox1.Text += button.Text
+                    isOPrClick = False
+                End If
+
+            ElseIf Not button.Text.Equals(".") Then
+            TextBox1.Text += button.Text
+            isOPrClick = False
+            End If
+        Else
+            If oprClickCount = 0 Then
+                oprClickCount += 1
+                n1 = Double.Parse(TextBox1.Text)
+                opr = button.Text
+                isOPrClick = True
+
+
+            Else
+                If Not button.Text.Equals("=") Then
+                    If Not isEqualClick Then
+                        n2 = Double.Parse(TextBox1.Text)
+                        TextBox1.Text = Convert.ToString(Calculate(opr, n1, n2))
+                        n2 = Double.Parse(TextBox1.Text)
+                        opr = button.Text
+                        isOPrClick = True
+                        isEqualClick = False
+
+                    Else
+                        isEqualClick = False
+                        opr = button.Text
+                    End If
+
+                Else
+                    n2 = Double.Parse(TextBox1.Text)
+                    TextBox1.Text = Convert.ToString(Calculate(opr, n1, n2))
+                    n1 = Double.Parse(TextBox1.Text)
+                    isOPrClick = True
+                    isEqualClick = True
+                End If
+
+            End If
+
+        End If
+
+
+
+
+    End Sub
+
+
+    Function IsOperator(ByVal btn As Button) As Boolean
+        Dim btnText As String
+        btnText = btn.Text
+        If (btnText.Equals("+") Or btnText.Equals("-") Or btnText.Equals("/") Or btnText.Equals("X") Or btnText.Equals("=")) Then
+            Return True
+        Else
+            Return False
+
+        End If
+    End Function
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        TextBox1.Text = ""
+    End Sub
+
+    Function Calculate(ByVal opr As String, ByVal num1 As Double, ByVal num2 As Double) As Double
+
+        Dim result As Double
+        result = 0
+
+        Select Case opr
+            Case "+"
+                result = num1 + num2
+            Case "-"
+                result = num1 - num2
+            Case "X"
+                result = num1 * num2
+            Case "/"
+                If num2 <> 0 Then
+                    result = num1 / num2
+                End If
+
+        End Select
+        Return result
+    End Function
+
+End Class
